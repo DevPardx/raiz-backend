870be79906af498dc211d64faa65eb4bdd7ba619
DevPardx <engineer.pardo@gmail.com>
2025-12-14 10:57:51 -0600

    fix: add missing env to tests setup

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -3,3 +3,6 @@ import "reflect-metadata";
 process.env.NODE_ENV = "test";
 process.env.JWT_SECRET = "test-jwt-secret";
 process.env.JWT_REFRESH_SECRET = "test-jwt-refresh-secret";
+process.env.CLOUDINARY_API_KEY = "cloudinary_api_key";
+process.env.CLOUDINARY_API_SECRET = "cloudinary_api_secret";
+process.env.CLOUDINARY_CLOUD_NAME = "cloudinary_cloud_name";
